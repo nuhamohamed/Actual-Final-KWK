bc747325ac191e07caed4cddb0510026bc75c6b2
Nuha <nuhasafia123@gmail.com>
2023-07-28 10:29:25 -0700

    Initial Commit

---
+++ Actual Final KWK/Actual_Final_KWKApp.swift
@@ -0,0 +1,17 @@
+//
+//  Actual_Final_KWKApp.swift
+//  Actual Final KWK
+//
+//  Created by Nuha Mohamed on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Actual_Final_KWKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
